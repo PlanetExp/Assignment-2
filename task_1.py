Task 1: Fixed the feature scaling function Task 2: Added some testing result Task 5: Updated grid search function              Added plot code base on C and gamma(Test needed)

task_1.py
@@ -1,5 +1,7 @@
+from __future__ import division
+
 import pickle
-from sklearn.model_selection import train_test_split,cross_val_score,KFold
+from sklearn.model_selection import train_test_split,cross_val_score,KFold, StratifiedShuffleSplit
 from sklearn import  svm
 from sklearn import preprocessing
 from sklearn.pipeline import make_pipeline
@@ -9,7 +11,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 import scipy.stats.stats as st
-
+from sklearn.base import TransformerMixin,BaseEstimator
+from sklearn.utils.validation import check_is_fitted
+from sklearn.utils import check_array,as_float_array
+from scipy import linalg
+from sklearn.decomposition import  PCA
 
 """ Import data set"""
 with open('forest_data.pickle', 'rb') as f:
@@ -33,7 +39,32 @@ class DataProcessing():
     seed = 42;
 
     def __init__(self):
+        # Let us shuffle the data set
+        #np.random.shuffle()
         self.X_train,self.X_test,self.y_train,self.y_test = train_test_split(data,target,test_size=self.test_size,random_state=self.seed)
+
+
+    def shuffle_dataset(self):
+
+        # Shuffle the dataset
+        np.random.shuffle(input_dataset)
+
+        # Prepare a stratisfied train and test split
+        train_size = 0.85
+        test_size = 1 - train_size
+
+        # For ease we merge the data and target
+        input_dataset = np.column_stack([data, target])
+
+        stratified_split = StratifiedShuffleSplit(input_dataset[:,-1],test_size=test_size,n_splits=1)
+
+        for train_indx, test_indx in stratified_split:
+            X_train = input_dataset[train_indx,:-1]
+            y_train = input_dataset[train_indx,-1]
+            X_test = input_dataset[test_indx,:-1]
+            y_test = input_dataset[test_indx,-1]
+
+        return  X_train,y_train,X_test,y_test
 
     def check_missing_value(self):
         """ Check missing value of data set
@@ -129,17 +160,17 @@ class DataProcessing():
             Transform X_train and X_test then return them
         """
         scaler = preprocessing.StandardScaler()
-        X_train_scale = scaler.fit_transform(self.X_train[:,:10])
-        X_test_scale = scaler.fit_transform(self.X_test[:,:10])
-        return X_train_scale,X_test_scale
+        X_train_scale = scaler.fit_transform(self.X_train)
+        X_test_scale = scaler.transform(self.X_test)
+        return X_train_scale, X_test_scale
 
     def normalize(self):
         """ Scaling feature using Normalize
             Transform X_train and X_test then return them
         """
-        normalizer = preprocessing.Normalizer(norm='l1')
-        X_train_scale = normalizer.fit_transform(self.X_train[:,:10])
-        X_test_scale = normalizer.fit_transform(self.X_test[:,:10])
+        normalizer = preprocessing.Normalizer(norm='l2')
+        X_train_scale = normalizer.fit_transform(self.X_train)
+        X_test_scale = normalizer.transform(self.X_test)
         return X_train_scale, X_test_scale
 
     def min_max_scaler(self):
@@ -147,8 +178,8 @@ class DataProcessing():
             Transform X_train and X_test then return them
         """
         min_max_scaler = preprocessing.MinMaxScaler()
-        X_train_scale = min_max_scaler.fit_transform(self.X_train[:,:10])
-        X_test_scale = min_max_scaler.fit_transform(self.X_test[:,:10])
+        X_train_scale = min_max_scaler.fit_transform(self.X_train)
+        X_test_scale = min_max_scaler.transform(self.X_test)
         return X_train_scale, X_test_scale
 
     def maxAbs_scaler(self):
@@ -156,8 +187,8 @@ class DataProcessing():
             Transform X_train and X_test then return them
         """
         maxAbs = preprocessing.MaxAbsScaler()
-        X_train_scale = maxAbs.fit_transform(self.X_train[:,:10])
-        X_test_scale = maxAbs.fit_transform(self.X_test[:,:10])
+        X_train_scale = maxAbs.fit_transform(self.X_train)
+        X_test_scale = maxAbs.transform(self.X_test)
         return X_train_scale, X_test_scale
 
 def Save_Classifier(clf,file_name):
@@ -173,7 +204,7 @@ def Load_Classifier(file_name):
     """
     return joblib.load(file_name)
 
-#a = DataProcessing()
+a = DataProcessing()
 
 # Plot the class distribution
 #a.plot_features()

task_5.py
@@ -6,6 +6,7 @@ import sklearn as sk
 import numpy as np
 import  matplotlib.pyplot as plt
 from sklearn.svm import SVC
+from sklearn.model_selection import  GridSearchCV
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.preprocessing import  scale
 from sklearn.model_selection import train_test_split
@@ -18,43 +19,39 @@ data = DataProcessing()
 # Get the transformed data
 X_train_scale,X_test_scale = data.standard_scaler()
 
-# Gaussian Naive-Bayes with no calibration
-clf = SVC(kernel='linear',C=1)
+clf_ = SVC(kernel='rbf')
+
+# Choose cross-validation iterator
+from sklearn.model_selection import ShuffleSplit
+cv = ShuffleSplit(n_splits=10,test_size=0.2,random_state=0)
+
+# Set up C and gamma
+Cs = [0.1, 1,10,100,1000]
+Gammas = np.logspace(-6,-1,10)
+
+param_grid = dict(svm__C = Cs,
+                svm__gammas = Gammas)
+
+grid_seach(clf_,param_grid,cv,X_train_scale,data.y_train,X_test_scale,data.y_test)
 
 
 
-# # this dataset is way too high-dimensional. Better do PCA:
-# pca = PCA(n_components=2)
-#
-# # Select feature
-# selection = SelectKBest(k=1)
-#
-# # Build estimator from PCA and Univariate selection:
-# combine_features = FeatureUnion([("pca",pca),("univ_select",selection)])
-#
-# # Use combine features to transform dataset:
-# X_features = combine_features.fit(X_train,y_train)
-#
-# clf = SVC(kernel='linear')
-#
-#
-# # Choose cross-validation iterator
-# from sklearn.model_selection import ShuffleSplit
-# cv = ShuffleSplit(n_splits=10,test_size=0.2,random_state=0)
-#
-# # Do grid search over k,n_components and C
-# pipeline = Pipeline([(("features", combine_features),("clf",clf))])
-#
-# param_grid = dict(features__pca__n_components = [1,2,3],
-#                   features__univ_select__k = [1,2],
-#                   svm__C = [0.1,1,10],
-#                   svm__gammas = np.logspace(-6,-1,10))
-# gs = GridSearchCV(pipeline,cv=cv,param_grid=param_grid, verbose=10)
-# gs.fit(X_train,y_train)
-#
-# print(gs.best_estimator_)
-#
-#
+"""Plot the score base on C and gamma"""
+# scores = [x[1] for x in gs.cv_results_['mean_test_score']]
+# scores_std = [x[1] for x in gs.cv_results_['std_test_score']]
+# scores = np.array(scores).reshape(len(Cs),len(Gammas))
+# scores_std = np.array(scores_std).reshape(len(Cs),len(Gammas))
+
+# for ind,i in enumerate(Cs):
+#     plt.subplot(2,1,1)
+#     plt.plot(Gammas,scores[ind],'-o',label = 'C: ' +str(i))
+#     plt.subplot(2,1,2)
+#     plt.plot(Gammas, scores_std[ind],'-o', label='C: ' + str(i))
+# plt.legend()
+# plt.xlabel('Gamma')
+# plt.ylabel('Mean score')
+# plt.show()
+
 # # Debug algorithm with learning curve
 # from sklearn.model_selection import learning_curve
 # title = 'Learning Curves (SVM, Linear kernel, $\gamma=%.6f$)' % gs.cv_results_['param_gamma']
@@ -62,6 +59,7 @@ clf = SVC(kernel='linear',C=1)
 # plot_learning_curve(estimator,title,X_train,y_train,cv=cv)
 # plt.show()
 # gs.score(X_test,y_test)
+
 
 
 